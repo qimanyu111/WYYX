75bbb404a685caab05e9e4c138350e59cb021425
qimanyu <1633079543@qq.com>
2020-05-30 09:48:34 +0800

    项feat:项目创建

---
+++ wyyx/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    1
+  </div>
+</template>
+<script>
+export default {
+  name: "App",
+  // 注册组件
+  components: {},
+};
+</script>
+<style lang="less" rel="stylesheet/less" scoped></style>
